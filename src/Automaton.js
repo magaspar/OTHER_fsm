e03311fa375f3dbe866ee710b59b5fe1e0546a94
bilow <simon.guilliams@gmail.com>
2020-02-09 14:30:08 +0100

    Initial commit

---
+++ src/Automaton.js
@@ -0,0 +1,12 @@
+export default class Automaton {
+  constructor(props) {
+    // Implement this
+    // Do any setup you think you need for the Automaton to work
+
+  }
+
+
+  readCommands(commands) {
+	  
+  }
+}
