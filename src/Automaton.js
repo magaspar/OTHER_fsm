a04cbaa7b0967b83957705c7d4e311dbf405c81c
Simon <sguillia@users.noreply.github.com>
2020-03-06 17:38:41 +0100

    Update Automaton.js

--- src/Automaton.js
+++ src/Automaton.js
@@ -2,11 +2,12 @@ export default class Automaton {
   constructor(props) {
     // Implement this
     // Do any setup you think you need for the Automaton to work
-
   }
 
 
   readCommands(commands) {
-	  
+    // Implement this
+    // Read in an commands ([String])
+    // Return true if we end in our accept state, false otherwise.  
   }
 }
